470363a40a0aafd64188b0fb75a05898d1cf4e81
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-10-10 12:53:45 +0300

    Добавьте файлы проекта.

---
+++ Stek/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var stack = new Stack<int>();
+
+           
+            stack.Push(2);
+            stack.Push(19);
+            stack.Push(34);
+            stack.Push(245);
+            stack.Push(2490);
+            Console.Write($"В стек є {stack.Count} елементів.");
+
+            
+            var item1 = stack.Pop();
+            Console.WriteLine($"Верхній елемент {item1}.");
+            var item2 = stack.Pop();
+            Console.WriteLine($"Перед останній елеметн {item2}.");
+
+           
+            stack.Push(455);
+            var item3 = stack.Peek();
+            Console.WriteLine($"Новий верхній елемент {item3}.");
+            var item4 = stack.Peek();
+            Console.WriteLine($"Новий верхній елемент {item4}.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stek/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+   
+    public class Stack<T>
+    {
+       
+        private List<T> _items = new List<T>();
+
+        public int Count => _items.Count;
+
+       
+        public void Push(T item)
+        {
+            
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+           
+            _items.Add(item);
+        }
+
+      
+        public T Pop()
+        {
+           
+            var item = _items.LastOrDefault();
+
+           
+            if (item == null)
+            {
+                throw new NullReferenceException("Стек пустий. Нема елементів для получення.");
+            }
+
+           
+            _items.RemoveAt(_items.Count - 1);
+
+          
+            return item;
+        }
+
+       
+        public T Peek()
+        {
+            
+            var item = _items.LastOrDefault();
+
+           
+            if (item == null)
+            {
+                throw new NullReferenceException("Стек пустий. Нема елементів для получення.");
+            }
+
+            return item;
+        }
+    }
+}
